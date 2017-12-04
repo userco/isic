e74d8266094f4ffb153291d6f57c34fb2ed9093b
mpenelova <mpenelova@ucc.uni-sofia.bg>
2017-12-04 13:32:18 +0200

    LOgin: Work in process

---
+++ src/ISICBundle/Controller/SecurityController.php
@@ -0,0 +1,30 @@
+<?php
+// src/Controller/SecurityController.php
+namespace  ISICBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+    	$authUtils = $this->get('security.authentication_utils');
+
+    	 // get the login error if there is one
+	    $error = $authUtils->getLastAuthenticationError();
+
+	    // last username entered by the user
+	    $lastUsername = $authUtils->getLastUsername();
+
+	    return $this->render('security/login.html.twig', array(
+	        'last_username' => $lastUsername,
+	        'error'         => $error,
+	    ));
+    }
+}
