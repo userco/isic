45bc288859a142963d4718ac67be88b37e29686d
mpenelova <mpenelova@ucc.uni-sofia.bg>
2017-12-04 15:19:32 +0200

    Role and Permissions

---
+++ src/ISICBundle/Entity/Role.php
@@ -0,0 +1,22 @@
+<?php
+namespace ISICBundle\Entity;
+
+class Role{
+	
+	protected $id;
+	protected $name;
+
+	public function getId(){
+		return $this->id;
+	}
+	public function setId($id){
+		$this->id = $id;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+	public function setName($name){
+		$this->name = $name;
+	}
+}
