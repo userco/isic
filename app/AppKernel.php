e7eefd65e4d50634ffa1e42a91a63958f79a4ff7
vvalkanov <pc1@valkanov.ucc.uni-sofia.bg>
2017-12-06 13:54:50 +0200

    redis and resque and /import isic

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new ISICBundle\ISICBundle(),
             new Liuggio\ExcelBundle\LiuggioExcelBundle(),
+            new BCC\ResqueBundle\BCCResqueBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
